f18dc2500221e6b1cc81f1abd084a4aeb65bf56f
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-10-03 19:07:03 +0530

    Final Banana Ready

--- js/app.js
+++ js/app.js
@@ -2,20 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function constructurl(text){
+function constructUrl(text){
     return "https://api.funtranslations.com/translate/minion.json"+"?"+"text=" + text;
 }
 
-function dofetch(text){
-    console.log(constructurl(text))
-    fetch(constructurl(text))
+/** For Fetch Url and Call API and Get Response**/
+function doFetch(text){
+    fetch(constructUrl(text))
     .then(response => response.json())
     .then(json => outputDiv.innerText = json.contents.translated )
 }
+/** Event Handler Function**/
 function clickEventHandler(){
-    dofetch(txtInput.value);
+    doFetch(txtInput.value);
    
 }
+
+/** On Button click Call EventHander Function**/
 btnTranslate.addEventListener("click",clickEventHandler);
 
 
