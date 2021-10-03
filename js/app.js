13d3d4ac41590e962be5f091f957a6cce2f2a9d4
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-10-03 17:39:44 +0530

    banana updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-function clickEventHandler(){
-    console.log("Clicked");
-    console.log("input",txtInput.value);
+function constructurl(text){
+    return "https://api.funtranslations.com/translate/minion.json"+"?"+"text=" + text;
 }
-btnTranslate.addEventListener("click",clickEventHandler)
+
+function dofetch(text){
+    console.log(constructurl(text))
+    fetch(constructurl(text))
+    .then(response => response.json())
+    .then(json => outputDiv.innerText = json.contents.translated )
+}
+function clickEventHandler(){
+    dofetch(txtInput.value);
+   
+}
+btnTranslate.addEventListener("click",clickEventHandler);
+
+
+
